a701dd118dc79b40c2994c5dff7f1c4a16b9186e
gensduvoyage <gensduvoyage@users.noreply.github.com>
2025-06-19 18:10:00 +0200

    types

--- types.d.ts
+++ types.d.ts
@@ -1,4 +1,14 @@
 declare global {
+	type AllKeysPresent<T, U extends readonly (keyof T)[]> =
+		Exclude<keyof T, U[number]> extends never
+			? true
+			: ['❌ Missing keys', Exclude<keyof T, U[number]>];
+
+	type AllValuesPresent<T, U extends readonly T[]> =
+		Exclude<T, U[number]> extends never
+			? true
+			: ['❌ Missing values', Exclude<T, U[number]>];
+
 	type VoteType = 'Pour' | 'Contre';
 	interface Vote {
 		type: VoteType;
@@ -6,6 +16,11 @@ declare global {
 	}
 	interface VoteWithIP extends Vote {
 		IP: string;
+	}
+
+	interface ApiRoutes {
+		'my-ip': {ip: string};
+		vote: {};
 	}
 }
 
